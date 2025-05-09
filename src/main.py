e389195ffa09c65f5a772988fac951a3cd28546c
Salvador Cantuarias <salvador.cantuarias@students.iaac.net>
2025-05-09 14:35:06 +0200

    created child class for cardinal buoy detection. added json format data output for software_gps_path team

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from vision import visionNav as vn
+from vision import CardinalBuoys as cb
 import cv2 as cv
 
 # files
@@ -8,13 +9,25 @@ green = cv.VideoCapture('/home/salvador_cb/3_term/engineering_club/data/Videos/v
 red = cv.VideoCapture('/home/salvador_cb/3_term/engineering_club/data/Videos/video with only red bouyg.mp4')
 empty = cv.VideoCapture('/home/salvador_cb/3_term/engineering_club/data/Videos/empty sea.mp4')
 
-simulation = cv.VideoCapture('/home/salvador_cb/3_term/engineering_club/data/Videos/path 2.mp4')
+simulation_buoys = cv.VideoCapture('/home/salvador_cb/3_term/engineering_club/data/Videos/path 2.mp4')
 
-output = "/home/salvador_cb/3_term/engineering_club/data/output/output2.mp4"
+simulation_cardinals = cv.VideoCapture('/home/salvador_cb/3_term/engineering_club/data/Videos/cradinal_buoys_path ‐ Hecho con Clipchamp.mp4')
 
+output = "/home/salvador_cb/3_term/engineering_club/data/output/red_output_exp.mp4"
+
+# cardinal images
+north = cv.imread('/home/salvador_cb/3_term/engineering_club/data/img/cardinals/north_buoy.png')
+south = cv.imread('/home/salvador_cb/3_term/engineering_club/data/img/cardinals/south_buoy.png')
+east = cv.imread('/home/salvador_cb/3_term/engineering_club/data/img/cardinals/crop_east.png')
+west = cv.imread('/home/salvador_cb/3_term/engineering_club/data/img/cardinals/crop_west.png')
+
+cardinals = [east, west]
 #MAIN
 
 if __name__ == "__main__":
     
-    nav = vn(video=simulation)
-    nav.run_on_video(output_path=output)
+    '''nav = vn(video=simulation)
+    nav.run_on_video(output_path=output)'''
+
+    vision = cb(cardinals,video=simulation_cardinals)
+    vision.run_on_video(output)
--- src/vision.py
+++ src/vision.py
@@ -4,7 +4,6 @@ import numpy as np
 class visionNav:
     def __init__(self, video=None):
 
-        #inputs
         self.video = video
         self.image = None
 
@@ -67,7 +66,7 @@ class visionNav:
         x,y,w,h= cv.boundingRect(biggest_contour)
         position = x + w // 2
         box = cv.rectangle(self.image, (x,y), (x+w, y+h), color, 5)
-        cv.putText(self.image, f"{description} BUOY", (x, y - 10), cv.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+        cv.putText(self.image, f"{description} BUOY \n width:{w} height:{h}", (x, y - 10), cv.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
         return (True, position, x, y, w, h)
     
     def line_style(self, centerp, p1, p2, color, thickness, description):
@@ -87,7 +86,10 @@ class visionNav:
         if distance < 0:
 
             if x_green == 0 and y_green == 0:
-                self.line_style(green_missing,green_missing,red_point,(0, 255, 0),3,"Turn Starboard!") 
+                if x_red == 0 and y_red == 0:
+                    self.line_style(red_missing,red_missing,green_missing,(0, 0, 0),1,"CLEAR! FOLLOW GPS!")
+                else:
+                    self.line_style(green_missing,green_missing,red_point,(0, 255, 0),3,"Turn Starboard!") 
             
             else:
                 # Create points correctly with brackets
@@ -103,10 +105,63 @@ class visionNav:
                 cv.line(self.image,(((x_green + (green_w // 2)) + (x_red + (red_w // 2))) // 2, self.height // 2),(self.width // 2, self.height // 2), (0, 255, 255),2)
         else:
             if x_red == 0 and y_red == 0:
-                self.line_style(red_missing,green_point,red_missing,(0, 0, 255),3,"Turn Port!")
+                if x_green == 0 and y_green == 0:
+                    self.line_style(green_missing,green_missing,red_missing,(0, 0, 0),1,"CLEAR! FOLLOW GPS!")
+                else:
+                    self.line_style(red_missing,green_point,red_missing,(0, 0, 255),3,"Turn Port!")
             else:
                 self.line_style(center_line,green_point, red_point, (0, 0, 255), 3, f"Turn Around!")
             
+    def json(self, gx, gy, gw, gh, gtype, rx, ry, rw, rh, rtype):
+        if rx == 0 and ry == 0:
+            data = {
+                "green_buoy": {
+                    "x": gx,
+                    "y": gy,
+                    "width": gw,
+                    "height": gh,
+                    "type": gtype
+                }
+            }
+            text = f"GREEN BUOY:\nx: {data['green_buoy']['x']}\ny: {data['green_buoy']['y']}\nsize: {data['green_buoy']['width']}x{data['green_buoy']['height']}"
+        elif gx == 0 and gy == 0:
+            data = {
+                "red_buoy": {
+                    "x": rx,
+                    "y": ry,
+                    "width": rw,
+                    "height": rh,
+                    "type": rtype
+                }
+            }
+            text = f"RED BUOY:\nx: {data['red_buoy']['x']}\ny: {data['red_buoy']['y']}\nsize: {data['red_buoy']['width']}x{data['red_buoy']['height']}"
+        else:
+            data = {
+                "green_buoy": {
+                    "x": gx,
+                    "y": gy,
+                    "width": gw,
+                    "height": gh,
+                    "type": gtype
+                },
+                "red_buoy": {
+                    "x": rx,
+                    "y": ry,
+                    "width": rw,
+                    "height": rh,
+                    "type": rtype
+                }
+            }
+            text = f"GREEN BUOY:\nx: {data['green_buoy']['x']}\ny: {data['green_buoy']['y']}\nsize: {data['green_buoy']['width']}x{data['green_buoy']['height']}\n\nRED BUOY:\nx: {data['red_buoy']['x']}\ny: {data['red_buoy']['y']}\nsize: {data['red_buoy']['width']}x{data['red_buoy']['height']}"
+        return text, data
+
+    def put_text_multiline(self,img, text, org, font, font_scale, color, thickness=1, line_spacing=10):
+        x, y = org
+        for i, line in enumerate(text.split('\n')):
+            y_pos = y + i * (line_spacing + thickness)
+            cv.putText(img, line, (x, y_pos), font, font_scale, color, thickness)
+        return img
+
     def detect_buoys(self, min_area = 1000):
 
         green_detected, green_position, green_x, green_y, green_w, green_h = self.detect(self.mask_g, min_area, (0, 255, 0),"GREEN") if self.detect(self.mask_g, min_area, (0, 255, 0), "GREEN") else (False, None)
@@ -120,6 +175,16 @@ class visionNav:
         
         middle_frame = width // 2
         distance = middle_frame - self.middle_x
+
+        text, data = self.json(green_x, green_y, green_w, green_h, "GREEN", red_x, red_y, red_w, red_h, "RED")
+
+        text_lines = text.count('\n') + 1
+        rect_height = text_lines * 20 + 10
+        cv.rectangle(self.image, (10, 10), (200, 10 + rect_height), (0, 0, 0), -1)
+
+        # Draw the text
+        self.put_text_multiline(self.image, text, (15, 30), 
+                        cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, 20)
     
     def run_on_video(self, output_path):
         width = int(self.video.get(cv.CAP_PROP_FRAME_WIDTH))
@@ -148,3 +213,110 @@ class visionNav:
             cv.destroyAllWindows()
 
         return output_path
+    
+
+    
+class CardinalBuoys(visionNav):
+    def __init__(self, sift_images, video=None):
+        super().__init__(video)
+        self.sift_images = sift_images
+        self.y_mask = None
+        self.b_mask = None
+
+    def turn_gray(self, image):
+        gray_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+        return gray_image
+    
+
+    def detect_cardinal(self,frame, img_buoy):
+
+        sift = cv.SIFT_create(
+            nfeatures=0,
+            contrastThreshold=0.03,
+            edgeThreshold=10,
+            sigma=1.6,
+        )
+        bf = cv.BFMatcher()
+
+        img_matches = None
+        M = None
+        gray_buoy = self.turn_gray(img_buoy)
+        gray_frame = self.turn_gray(frame)
+
+        kp1, des1 = sift.detectAndCompute(gray_buoy, None)
+        kp2, des2 = sift.detectAndCompute(gray_frame, None)
+
+        if des2 is not None:
+            
+            matches = bf.knnMatch(des1, des2, k=2)
+
+            good_matches = [m for m, n in matches if m.distance < 0.8 * n.distance]
+
+            if len(good_matches) > 10:
+                
+                img_matches = cv.drawMatches(img_buoy, kp1, gray_frame, kp2, good_matches, None, flags=2) # cv2.drawMatches
+
+                # keypoints
+                src_pts = np.float32([kp1[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+                dst_pts = np.float32([kp2[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+
+                # Calculate homography
+                M, mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC, 5.0)
+
+        return img_matches, M
+    
+    def draw_buoy(self, frame, M, img_buoy, desc):
+        # If there are enough matches
+        if M is not None:
+            h, w = img_buoy.shape[:2]
+
+            # Define the four corners of the robot image
+            pts = np.float32([[0, 0], [w, 0], [w, h], [0, h]]).reshape(-1, 1, 2)
+
+            # Transform the points to locate the robot in the video frame
+            dst = cv.perspectiveTransform(pts, M)
+
+            # Draw a blue polygon around the detected robot in the frame
+            frame = cv.polylines(frame, [np.int32(dst)], True, (255, 0, 0), 3, cv.LINE_AA)
+            cv.putText(frame, desc, (int(dst[0][0][0]), int(dst[0][0][1])), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv.LINE_AA)
+
+        return frame
+    
+    def run_on_video(self, output_path):
+        width = int(self.video.get(cv.CAP_PROP_FRAME_WIDTH))
+        height = int(self.video.get(cv.CAP_PROP_FRAME_HEIGHT))
+        fps = self.video.get(cv.CAP_PROP_FPS)
+
+        fourcc = cv.VideoWriter_fourcc(*'mp4v')
+        out = cv.VideoWriter(output_path, fourcc, fps, (width, height))
+
+        try:
+            while self.video.isOpened():
+                ret, frame = self.video.read()
+                if not ret:
+                    break
+                self.image = frame
+
+                img_matches_e, M_e = self.detect_cardinal(frame, self.sift_images[0])
+                img_matches_w, M_w = self.detect_cardinal(frame, self.sift_images[1])
+
+                frame =  self.draw_buoy(frame, M_e, self.sift_images[0], "EAST BUOY")
+                frame =  self.draw_buoy(frame, M_w, self.sift_images[1], "WEST BUOY")
+
+                self.generate_masks()
+                self.detect_buoys()
+
+                out.write(self.image)
+                cv.imshow("Processed Frame", self.image)
+                if cv.waitKey(1) & 0xFF == ord('q'):
+                    break
+        
+        finally:
+            self.video.release()
+            out.release()
+            cv.destroyAllWindows()
+
+        return output_path
+
+
+########################################################################################
